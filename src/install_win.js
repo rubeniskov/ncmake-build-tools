ac62b0da27b5fb1829707df9f80eeff98aecec8e
Rubén López Gomez <me@rubeniskov.com>
2017-08-29 20:01:26 +0200

    update: win installer

--- src/install_win.js
+++ src/install_win.js
@@ -11,7 +11,9 @@ export default() => {
                     console.log('--- Downloading Net Framework 4.6');
                     return tools.download('https://download.microsoft.com/download/E/4/1/E4173890-A24A-4936-9FC9-AF930FE3FA40/NDP461-KB3102436-x86-x64-AllOS-ENU.exe', tools.tmpDir(), 'net_installer.exe').then((filepath) => {
                         console.log(`--- Installing ${filepath}`)
-                        return tools.sudo(filepath, {name: 'Install Net Framework'});
+                        return tools.sudo(`powershell -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -Command "& {&Start-Process -FilePath '${filepath}' -ArgumentList '/passive' -Wait}"`, {name: 'Install Net Framework'}).catch((err) => {
+                            return tools.sudo(`${filepath} /passive`, {name: 'Install Net Framework'});
+                        });
                     });
                 }
             });
@@ -24,7 +26,9 @@ export default() => {
                 } else {
                     tools.download('https://download.microsoft.com/download/5/f/7/5f7acaeb-8363-451f-9425-68a90f98b238/visualcppbuildtools_full.exe', tools.tmpDir(), 'visualcppbuildtools.exe').then((filepath) => {
                         console.log(`--- Installing ${filepath}`)
-                        return tools.sudo(filepath, {name: 'Install Visual CPP Build Tools'});
+                        return tools.sudo(`powershell -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -Command "& {&Start-Process -FilePath '${filepath}' -ArgumentList '/passive' -Wait}"`, {name: 'Install Visual CPP Build Tools'}).catch((err) => {
+                            return tools.sudo(`${filepath} /passive`, {name: 'Install Net Framework'});
+                        });;
                     });
                 }
             })
